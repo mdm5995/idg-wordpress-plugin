de416428e083944ed328d18a60b22d1b8bb3f263
Morgan <mdm5995@gmail.com>
2023-07-11 11:53:15 -0400

    Create API call for projects data

--- wordpress/wp-content/plugins/idg-project-v2/src/ProjectsArchive.js
+++ wordpress/wp-content/plugins/idg-project-v2/src/ProjectsArchive.js
@@ -17,6 +17,64 @@ export default function ProjectsArchive() {
 		);
 	});
 
+	// TODO: add category handling
+	// need to update projects on category change 
+	// (NOTE: might cause weird infinite loop shit again!)
+
+	const [projects, setProjects] = useState([]);
+	const [category, setCategory] = useState('all');
+
+	const getProjectsData = () => {
+		const projectsArray = fetch('https://localhost/wp-json/wp/v2/projects?_embed')
+			.then((response) => response.json())
+			.then((json) => {
+				console.log('this is the json:');
+				console.log(json);
+				const projectsArray = json.map((project) => {
+					const projectObject = {
+						id: project.id,
+						title: project.title.rendered,
+						content: project.content.rendered,
+						link: project.link,
+						thumbnail: project['_embedded']['wp:featuredmedia'][0].source_url,
+						category: 'k12'
+					};
+					console.log('this is the projectobject:');
+					console.log(projectObject);
+					return projectObject;
+				});
+				console.log('this is the projectArray:');
+				console.log(projectsArray);
+				return projectsArray;
+			}).catch((e) => console.error('error!' + e));
+		return projectsArray;
+	};
+
+	useEffect(() => {
+		let ignore = false;
+		setProjects([]);
+		getProjectsData()
+		.then((projectsArray) => {
+			if (!ignore) {
+				setProjects(projectsArray);
+			};
+		});
+		return () => {
+			ignore = true;
+		};
+	}, []);
+
+	const projectsList = projects.map((project) => {
+			return (
+			<div className={`project-item ${project.category}`}>
+				<a href={project.link}>
+					<h1>{project.title}</h1>
+					<img src={project.thumbnail}/>
+				</a>
+			</div>
+		);
+	});
+
 	// TODO: 
 	// do i set up the functionality first or get the data first? probably get the data.
 	// figure out how to pull the actual projects data from the wp rest api. 
@@ -27,6 +85,9 @@ export default function ProjectsArchive() {
 			<section id='category-list'>
 				{categoriesList}
 			</section>
+			<div id='projects-grid'>
+				{projectsList}
+			</div>
 			<div id='projects-grid'>
 				<div className='project-item k12'>
 					<a>
