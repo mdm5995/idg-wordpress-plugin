ca2a63b10ac47618aedadae0533febee13ffafe5
Morgan <mdm5995@gmail.com>
2023-07-13 15:29:22 -0400

    Remove errant console.log()

--- wordpress/wp-content/plugins/idg-project-v2/src/index.js
+++ wordpress/wp-content/plugins/idg-project-v2/src/index.js
@@ -1,8 +1,6 @@
 import { createRoot, render, createElement } from '@wordpress/element';
 import ProjectsArchive from './ProjectsArchive.js';
 
-console.log('script successfully enqueued!');
-
 const projectsDomNode = document.getElementById('projects-page-container');
 
 createRoot(projectsDomNode).render(<ProjectsArchive />);
