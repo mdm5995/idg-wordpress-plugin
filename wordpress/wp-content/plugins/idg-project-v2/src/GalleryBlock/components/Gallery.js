3cf623902551a01f2a2f60de5f8efc2deeb775b2
Morgan <mdm5995@gmail.com>
2023-08-16 11:50:35 -0400

    Remove h3 tag from gallery images

--- wordpress/wp-content/plugins/idg-project-v2/src/GalleryBlock/components/Gallery.js
+++ wordpress/wp-content/plugins/idg-project-v2/src/GalleryBlock/components/Gallery.js
@@ -4,7 +4,6 @@ const Gallery = ({arrayOfImages, handleImageClick}) => {
 		(image) => {
 			return (
 				<div class="idg-gallery-image-item" onClick={handleImageClick}>
-					<h3>{image.caption}</h3>
 					<img key={image.id} src={image.src} alt={image.alt} data-id={image.id} />
 				</div>
 			);
