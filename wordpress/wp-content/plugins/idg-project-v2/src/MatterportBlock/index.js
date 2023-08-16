fcd99d8d172ada5f7522063b06b4e9d421dbcfad
Morgan <mdm5995@gmail.com>
2023-08-16 12:20:24 -0400

    Add editor css for matterport block

--- wordpress/wp-content/plugins/idg-project-v2/src/MatterportBlock/index.js
+++ wordpress/wp-content/plugins/idg-project-v2/src/MatterportBlock/index.js
@@ -1,6 +1,8 @@
 import { registerBlockType } from '@wordpress/blocks';
 import editMatterportBlock from './edit.js'
 import saveMatterportBlock from './save.js'
+import './style.css';
+import './editor-style.css';
 
 // IDG Matterport Block
 console.log('about to register matterport!');
